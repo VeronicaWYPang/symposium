ac6b14e7aacb74fe46bf99c8efa69362c78b5ac3
Jesús García <ctw@ctwhome.com>
2022-05-19 10:20:54 +0200

    remove nuxt previous version

--- nuxt/components/Logo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <nuxt-link to="/" class="text-white">
-    NLESC Conference'22
-  </nuxt-link>
-</template>
--- nuxt/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex flex-col h-screen ">
-    <main-header />
-    <Nuxt class="bg-base-200 flex-1" />
-  </div>
-</template>
